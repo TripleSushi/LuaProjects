ce0b3f512ab341c98807dd832ffc78710268712d
TripleSushi <csgovalve1222@gmail.com>
2025-07-23 17:18:00 -0600

    Bubblesort algorithm

--- bubblesort.lua
+++ bubblesort.lua
@@ -6,10 +6,10 @@ for i = 1, 10 do
     table.insert(tabla, math.random(1, 40))
 end
 
-for i = 1, #table - 1 do
+for i = 1, #tabla - 1 do
     for j = 1, #tabla - i do
         if tabla[j] > tabla[j + 1] then
-            local temp = table[j]
+            local temp = tabla[j]
 
             tabla[j] = tabla[j + 1]
             tabla[j + 1] = temp
